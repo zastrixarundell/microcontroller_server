f28ad9681f358e25129650e24360bb440a562e06
zastrixarundell <zastrixarundell@gmail.com>
2023-08-10 10:13:46 +0200

    Updated module and function documentation and specs.

--- lib/microcontroller_server/services/auth_services/clients/microcontroller_behaviour.ex
+++ lib/microcontroller_server/services/auth_services/clients/microcontroller_behaviour.ex
@@ -1,5 +1,13 @@
 defmodule MicrocourlntrollerServer.Services.AuthServices.Clients.MicrocontrollerBehaviour do
   alias HTTPoison.{Request, Error}
 
-  @callback get(String.t()) :: {:ok, %Request{}} | {:error, %Error{}}
+  @moduledoc """
+  Behaviour for the API requests for the microcontroller auth server. It's written this way
+  to be able to be mocked.
+  """
+
+  @doc """
+  Wrapper for HTTPoison get. It's written this way to be able to be mocked in tests.
+  """
+  @callback get(String.t()) :: {:ok, Request.t()} | {:error, Error.t()}
 end
--- lib/microcontroller_server/services/auth_services/clients/microcontroller_client.ex
+++ lib/microcontroller_server/services/auth_services/clients/microcontroller_client.ex
@@ -1,5 +1,9 @@
 defmodule MicrocourlntrollerServer.Services.AuthServices.Clients.MicrocontrollerClient do
   @behaviour MicrocourlntrollerServer.Services.AuthServices.Clients.MicrocontrollerBehaviour
 
+  @moduledoc """
+  Actual implementation of the HTTPoison request wrappers.
+  """
+
   def get(uri), do: HTTPoison.get(uri)
 end
