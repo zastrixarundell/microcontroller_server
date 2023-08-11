f0671f9b7b05f7e5b27b1373330ebc17cb36238b
zastrixarundell <zastrixarundell@gmail.com>
2023-08-11 04:13:39 +0200

    Add missing sensors

---
+++ lib/microcontroller_server/microcontroller/sensor.ex
@@ -0,0 +1,21 @@
+defmodule MicrocontrollerServer.Microcontroller.Sensor do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias MicrocontrollerServer.Microcontroller.{Device, Reading}
+
+  schema "sensors" do
+    field :name, :string
+    belongs_to :device, Device
+    has_many :readings, Reading
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(sensor, attrs) do
+    sensor
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
